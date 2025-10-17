f313131211bcf7b2649e7a82b45d7ced4d4f1159
seercle <notseercle@gmail.com>
2025-10-17 16:29:33 +0200

    fix time format

--- docker/common/mem.sh
+++ docker/common/mem.sh
@@ -1,19 +1,31 @@
 INTERVAL_SECONDS="$1"
-
+OUTPUT_FILE="$2"
+TIME_FORMAT="$3"
 if [ -z "$INTERVAL_SECONDS" ]; then
   echo "Error: INTERVAL_SECONDS must be provided."
-  echo "Usage: $0 <INTERVAL_SECONDS>"
+  echo "Usage: $0 <INTERVAL_SECONDS> <OUTPUT_FILE> <TIME_FORMAT>"
   exit 1
 fi
 
-OUTPUT_FILE="/OUTPUTS/memory_usage.csv"
+if [ -z "$OUTPUT_FILE" ]; then
+  echo "Error: OUTPUT_FILE must be provided."
+  echo "Usage: $0 <INTERVAL_SECONDS> <OUTPUT_FILE> <TIME_FORMAT>"
+  exit 1
+fi
+
+if [ -z "$TIME_FORMAT" ]; then
+  echo "Error: TIME_FORMAT must be provided."
+  echo "Usage: $0 <INTERVAL_SECONDS> <OUTPUT_FILE> <TIME_FORMAT>"
+  exit 1
+fi
+
 #if [ ! -f "$OUTPUT_FILE" ]; then
 #  echo "Creating output file: $OUTPUT_FILE"
 #  echo "timestamp,memory_usage_gib" > "$OUTPUT_FILE"
 #fi
 
 while true; do
-  TIMESTAMP=$(date +"%Y-%m-%d_%H:%M:%S")
+  TIMESTAMP=$(date +$TIME_FORMAT)
   MEMORY_USAGE_BYTES=$(cat /sys/fs/cgroup/memory.current)
   MEMORY_USAGE_GIB=$(awk "BEGIN {printf \"%.2f\", $MEMORY_USAGE_BYTES / 1073741824}")
   echo "$TIMESTAMP,$MEMORY_USAGE_GIB" >> "$OUTPUT_FILE"
--- docker/common/start.sh
+++ docker/common/start.sh
@@ -1,19 +1,20 @@
-BIN_BASH="/bin/bash"
-DATE_FORMAT="%Y-%m-%d_%H:%M:%S"
 if [ -z "$INTERVAL_SECONDS" ]; then
   echo "Error: INTERVAL_SECONDS must be provided."
   echo "Usage: $0 <INTERVAL_SECONDS>"
   exit 1
 fi
 
+BIN_BASH="/bin/bash"
+DATE_FORMAT="%Y-%m-%d_%H:%M:%S"
 LOG_FILE="/OUTPUTS/deep_brain_seg_logs.csv"
-#echo "timestamp,log" > "$LOG_FILE"
+MEMORY_USAGE_FILE="/OUTPUTS/memory_usage.csv"
+
 
 # Iterate over files in /CUSTOM_INPUTS
 find /CUSTOM_INPUTS -type f ! -path "*/.*" | sort | while read -r file; do
   if [ -f "$file" ]; then
-    START_TIMESTAMP=$(date +$DATE_FORMAT)
-    echo "$START_TIMESTAMP: Study of $(basename $file) started."
+    start_timestamp=$(date +%s)
+    echo "$(date -d @$start_timestamp +$DATE_FORMAT): Study of $(basename $file) started."
 
     # Clear the /INPUTS directory
     rm -rf /INPUTS/*
@@ -22,16 +23,16 @@ find /CUSTOM_INPUTS -type f ! -path "*/.*" | sort | while read -r file; do
     cp "$file" /INPUTS/
 
     # Start the memory monitoring script in the background
-    $BIN_BASH /opt/mem.sh "$INTERVAL_SECONDS" &
-    MEM_SCRIPT_PID=$!
+    $BIN_BASH /opt/mem.sh "$INTERVAL_SECONDS" "$MEMORY_USAGE_FILE" &
+    mem_script_pid=$!
 
-    $BIN_BASH /extra/run_deep_brain_seg.sh 2>&1 | while IFS= read -r LOG; do
-      TIMESTAMP=$(date +$DATE_FORMAT)
-      echo "$TIMESTAMP,$LOG" >> "$LOG_FILE"
+    $BIN_BASH /extra/run_deep_brain_seg.sh 2>&1 | while IFS= read -r log; do
+      timestamp=$(date +$DATE_FORMAT)
+      echo "$timestamp,$log" >> "$LOG_FILE"
     done
 
     # Stop the memory monitoring script
-    kill $MEM_SCRIPT_PID &>/dev/null
+    kill $mem_script_pid
 
     # Create a directory in /CUSTOM_OUTPUTS named after the file
     filename=$(basename "$file")
@@ -39,13 +40,13 @@ find /CUSTOM_INPUTS -type f ! -path "*/.*" | sort | while read -r file; do
     mkdir -p "$output_dir"
 
     # Move the content of /OUTPUTS to the new directory
-    mv /OUTPUTS/* "$output_dir/"
+    #mv /OUTPUTS/* "$output_dir/"
+    mv $LOG_FILE "$output_dir/"
+    mv $MEMORY_USAGE_FILE "$output_dir/"
 
-    END_TIMESTAMP=$(date +$DATE_FORMAT)
-    echo "$END_TIMESTAMP: Study of $(basename $file) ended."
+    end_timestamp=$(date +%s)
+    echo "$(date -d @$end_timestamp +$DATE_FORMAT): Study of $(basename $file) ended."
 
-    start_seconds=$(date -d "$START_TIMESTAMP" +%s)
-    end_seconds=$(date -d "$END_TIMESTAMP" +%s)
     elapsed_seconds=$((end_seconds - start_seconds))
     elapsed_minutes=$((elapsed_seconds / 60))
     echo "Elapsed time: $elapsed_minutes minutes"
